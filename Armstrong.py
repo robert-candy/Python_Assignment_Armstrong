5ecdf168a8c54f3c43083bfc7fa07b9fa0072659
robert-candy <candyrobertt@gmail.com>
2020-04-27 06:12:17 +0300

    commit

--- Armstrong.py
+++ Armstrong.py
@@ -2,28 +2,23 @@ import os
 clear = lambda: os.system("cls")
 clear()
 
-
-user_input = input("please enter a number  : ")
-user_input_list = list(user_input)
-
-
-sum = 0
-
-if int(user_input) > 0:
-    for i in user_input_list:
-        sum += (int(i)** len(user_input))
-    if sum == int(user_input):
-        print(sum, "is a Armstrong number")
+# print("Presss 'q' for quit.")
+while True:
+    number = input("Enter a positive Number:").strip().lower()
+    digits = len(number)
+    summ = 0
+    if number.isnumeric():
+        for i in range(digits):
+            summ += int(number[i]) ** digits
+        if summ == int(number):
+            print(number, "is a Amstrong Number.")
+            break
+        else:
+            print(number, "is not a Amstrong Number.")
+    elif number.count(".") == 1 and number.replace(".", "1").isnumeric() or number.count(",") == 1 \
+            and number.replace(",", "1").isnumeric():
+            print("Please enter a integer number.")
+    elif number.count("-") == 1 and number.index("-") == 0 and number.replace("-", "1").isnumeric():
+        print("Please enter a positive number")
     else:
-        print(user_input, "is not a Armstrong number")
-elif int(user_input) < 0:
-    print("Please enter a positive number ")
-
-
-
-
-
-
-
-
-
+        print("Do not use any entries other than numeric values")
